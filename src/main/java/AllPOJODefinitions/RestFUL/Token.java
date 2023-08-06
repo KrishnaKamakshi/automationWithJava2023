30eed30aa51765987b2281cc6aaf12452abd3cc4
KRISHNA KAMAKSHI BRAHMA <33858610+KrishnaKamakshi@users.noreply.github.com>
2023-08-06 14:10:11 +0530

    Logger Files

---
+++ src/main/java/AllPOJODefinitions/RestFUL/Token.java
@@ -0,0 +1,13 @@
+package AllPOJODefinitions.RestFUL;
+
+public class Token {
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    private String token;
+}
