3e90a30e96400ee0e5b011c6fe07ff232c1f9c47
KrishnaKamakshi <ckrishna416@gmail.com>
2024-04-12 15:15:36 +0530

    Build fixes

--- src/test/java/WebAutomationTestCases/DemoAutomation2.java
+++ src/test/java/WebAutomationTestCases/DemoAutomation2.java
@@ -1,5 +1,6 @@
 package WebAutomationTestCases;
 
+import BaseClass.BaseClass1;
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
@@ -10,14 +11,16 @@ import web.Steps.AgileTestingAllianceSteps;
 
 import java.util.concurrent.TimeUnit;
 
+import static driverClass.DriverFinal.driver;
 
-public class DemoAutomation2 extends AgileTestingAllianceSteps {
+
+public class DemoAutomation2 extends BaseClass1 {
     LoginPageFactory loginFactory = new LoginPageFactory();
     @Description("Navigation to Flipkart Page")
     @Severity(SeverityLevel.NORMAL)
     @Test(priority = 1)
     public void testUrlNavigationRahulShetty() throws InterruptedException {
-        getURL("https://courses.rahulshettyacademy.com/");
+        driver.get("https://courses.rahulshettyacademy.com/");
         loginFactory.clickOnLandingPage();
         WebDriver.Timeouts timeouts = driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         loginFactory.clickOnSignWithUpEmail();
