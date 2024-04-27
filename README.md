2de234d840d1994f79682fec2f533a6b3d19bcd4
KrishnaKamakshi <ckrishna416@gmail.com>
2024-04-27 10:18:17 +0530

    Selenium Grid

--- README.md
+++ README.md
@@ -13,4 +13,6 @@
                 AWS Instance Creation and uploading the Reports to S3 Bucket - Done
                 Add Screenshots to Allure Report and add directory for taking the screenshots - Done
                 Design Patterns - POM, Method Chaining, SingleTon, Factory, Abstract factory
-                Pages - added, Utility 
+                Pages - added, Utility - Done
+                Selenium Grid IT - Done 
+                Azure DevOps with Selenium Grid - Pipeline - Pending 
--- src/test/java/designPatternTest/testBookDesigWithFDP.java
+++ src/test/java/designPatternTest/testBookDesigWithFDP.java
@@ -2,15 +2,28 @@ package designPatternTest;
 
 import SRP.BasePages.TravelHomePage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class testBookDesigWithFDP {
     By selectionElement = By.id("flightSearchContainer");
     @Test
-    public void testFlightFooter() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+    public void testFlightFooter() throws InterruptedException, URISyntaxException, MalformedURLException {
+        DesiredCapabilities desCaps = new DesiredCapabilities();
+        desCaps.setBrowserName("chrome");
+        //desCaps.setPlatform(Platform.WIN11);
+        //desCaps.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+        WebDriver driver = new RemoteWebDriver(new URI("http://192.168.0.3:4444").toURL(), desCaps);
         driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
         TravelHomePage travelHomePage = new TravelHomePage(driver);
